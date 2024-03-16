b7a6b70dce19d19736db00f618723f51bc2f785a
Sylvia <sylviachebet03@gmail.com>
2024-03-16 17:48:59 +0300

    Finish bio/about section

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
-const p = document.createElement("p");
-p.textContent = "Sylvia Chebet is a Geospatial Engineering graduate";
-document.querySelector("body").appendChild(p);
+function onButtonClick() {
+    alert('My resume');
+  }
+  const button = document.querySelector('button');
+  button.addEventListener('click', onButtonClick);
+
+  
